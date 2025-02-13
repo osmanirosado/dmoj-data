694fef214c56047f1a8e64c9f18c97734a1fb0df
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-02-13 12:02:43 -0500

    More attributes for the OCI users view

--- oci_users.sql
+++ oci_users.sql
@@ -26,18 +26,23 @@ select concat('_', username) as name
 from auth_user
 where username regexp '^oci_[a-z]+[0-9]+$';
 
+drop view if exists oci_users;
+
 # OCI users mapping
 create view oci_users as
 select pu.username as primary_username,
        su.username as secondary_username,
        pp.id       as primary_profile_id,
-       sp.id       as secondary_profile_id
+       sp.id       as secondary_profile_id,
+       pu.id       as primary_user_id,
+       su.id       as secondary_user_id
 from auth_user pu
          join auth_user su on pu.username regexp '^oci_[a-z]+[0-9]+$' and su.username = concat('_', pu.username)
          join judge_profile pp on pu.id = pp.user_id
          join judge_profile sp on su.id = sp.user_id;
 
-select * from oci_users;
+select *
+from oci_users;
 
 # OCI users table
 create table oci_user_materialized
