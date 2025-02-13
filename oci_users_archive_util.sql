0b0052c2319da9f5de72d1fa25fc238f9d948ebb
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-02-13 11:35:49 -0500

    Queries to archive data of OCI users

---
+++ oci_users_archive_util.sql
@@ -0,0 +1,100 @@
+# Contest Participation #
+
+# Archived contest participation
+select count(*)
+from oci_user_materialized u
+         join judge_contestparticipation rel on u.secondary_profile_id = rel.user_id;
+
+# New contest participation
+select count(*)
+from oci_user_materialized u
+         join judge_contestparticipation rel on u.primary_profile_id = rel.user_id;
+
+# Archive new contest participation
+update judge_contestparticipation t
+set user_id = (select secondary_profile_id
+               from oci_user_materialized
+               where t.user_id = primary_profile_id)
+where user_id in (select primary_profile_id
+                  from oci_user_materialized);
+
+# Contest Banned Users #
+
+# Archived contest banned users
+select count(*) as archived_count
+from oci_user_materialized u
+         join judge_contest_banned_users rel on u.secondary_profile_id = rel.profile_id;
+
+# New contest banned users
+select count(*) as new_count
+from oci_user_materialized u
+         join judge_contest_banned_users rel on u.primary_profile_id = rel.profile_id;
+
+# Archive new banned users
+update judge_contest_banned_users t
+set profile_id = (select secondary_profile_id
+                  from oci_user_materialized
+                  where t.profile_id = primary_profile_id)
+where profile_id in (select primary_profile_id
+                     from oci_user_materialized);
+
+# Submissions #
+
+# Archived submissions
+select count(*) as archived_count
+from oci_user_materialized u
+         join judge_submission rel on u.secondary_profile_id = rel.user_id;
+
+# New submissions
+select count(*) as new_count
+from oci_user_materialized u
+         join judge_submission rel on u.primary_profile_id = rel.user_id;
+
+# Archive new submissions
+update judge_submission t
+set user_id = (select secondary_profile_id
+               from oci_user_materialized
+               where t.user_id = primary_profile_id)
+where user_id in (select primary_profile_id
+                  from oci_user_materialized);
+
+# Judge Ticket #
+
+# Archived judge tickets
+select count(*) as archived_count
+from oci_user_materialized u
+         join judge_ticket rel on u.secondary_profile_id = rel.user_id;
+
+# New judge tickets
+select count(*) as new_count
+from oci_user_materialized u
+         join judge_ticket rel on u.primary_profile_id = rel.user_id;
+
+# Archive new judge tickets
+update judge_ticket t
+set user_id = (select secondary_profile_id
+               from oci_user_materialized
+               where t.user_id = primary_profile_id)
+where user_id in (select primary_profile_id
+                  from oci_user_materialized);
+
+# Judge Ticket Message #
+
+# Archived judge ticket messages
+select count(*) as archived_count
+from oci_user_materialized u
+         join judge_ticketmessage rel on u.secondary_profile_id = rel.user_id;
+
+# New judge ticket messages
+select count(*) as new_count
+from oci_user_materialized u
+         join judge_ticketmessage rel on u.primary_profile_id = rel.user_id;
+
+# Archive new judge ticket messages
+update judge_ticketmessage t
+set user_id = (select secondary_profile_id
+               from oci_user_materialized
+               where t.user_id = primary_profile_id)
+where user_id in (select primary_profile_id
+                  from oci_user_materialized);
+
