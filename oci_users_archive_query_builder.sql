cccefd205a9c404137ce5985395ac5ce46cd179c
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-02-16 08:56:18 -0500

    Function to build a count query from a template

---
+++ oci_users_archive_query_builder.sql
@@ -0,0 +1,44 @@
+DELIMITER //
+
+CREATE FUNCTION build_count_query(IN referencing_table VARCHAR(255), IN referencing_column VARCHAR(255),
+                            IN referenced_table VARCHAR(255), IN referenced_column VARCHAR(255))
+    RETURNS VARCHAR(512)
+BEGIN
+    DECLARE result VARCHAR(512);
+
+    SET @template = '
+        SELECT COUNT(*) AS rows_count
+        FROM {{referenced_table}} d
+            JOIN {{referencing_table}} s ON d.{{referenced_column}} = s.{{referencing_column}};
+    ';
+
+    -- Replace placeholders with actual values
+    SET @sql = REPLACE(@template, '{{referenced_table}}', referenced_table);
+    SET @sql = REPLACE(@sql, '{{referenced_column}}', referenced_column);
+    SET @sql = REPLACE(@sql, '{{referencing_table}}', referencing_table);
+    SET @sql = REPLACE(@sql, '{{referencing_column}}', referencing_column);
+
+    SELECT @sql INTO result;
+    RETURN result;
+END //
+
+DELIMITER ;
+
+SELECT build_count_query('judge_contestparticipation', 'user_id',
+                   'oci_users', 'primary_profile_id') as query;
+
+SELECT build_count_query(TABLE_NAME,
+                   COLUMN_NAME,
+                   'oci_users',
+                   'primary_profile_id') as query
+FROM information_schema.KEY_COLUMN_USAGE
+WHERE REFERENCED_TABLE_NAME = 'judge_profile'
+  AND REFERENCED_TABLE_SCHEMA = 'dmoj';
+
+SELECT build_count_query(TABLE_NAME,
+                   COLUMN_NAME,
+                   'oci_users',
+                   'secondary_profile_id') as query
+FROM information_schema.KEY_COLUMN_USAGE
+WHERE REFERENCED_TABLE_NAME = 'judge_profile'
+  AND REFERENCED_TABLE_SCHEMA = 'dmoj';
