9f6e6740022add83c6e558b9bb4c6c6ccfe2c6d4
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-02-20 17:09:22 -0500

    Query moved to another file

--- users.sql
+++ users.sql
@@ -8,3 +8,12 @@ SELECT username, email, date_joined
 FROM auth_user
 WHERE date_joined >= DATE_SUB(CURDATE(), INTERVAL 1 DAY)
 ORDER BY date_joined DESC;
+
+-- Last login by user group
+select REGEXP_SUBSTR(username, '^oci_[a-z]+')               AS user_group,
+       CONVERT_TZ(MAX(last_login), 'UTC', 'America/Havana') AS group_last_login_cuba
+from auth_user
+where username regexp '^oci_[a-z]+[0-9]+$'
+group by user_group
+order by group_last_login_cuba desc;
+
