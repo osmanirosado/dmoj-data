21241ba30ef94285700ee1aeea3b7297f47587ce
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-02-20 17:04:38 -0500

    Some queries were deleted

--- users.sql
+++ users.sql
@@ -1,20 +1,10 @@
-
+-- Sample query
 select username, email, date_joined
 from auth_user
 order by date_joined desc;
 
+-- Users joined in the last day
 SELECT username, email, date_joined
 FROM auth_user
 WHERE date_joined >= DATE_SUB(CURDATE(), INTERVAL 1 DAY)
 ORDER BY date_joined DESC;
-
-select username
-from auth_user
-where username like '\_oci\_%';
-
-update auth_user
-set is_active = 0,
-    password = ''
-where username like '\_oci\_%';
-
-
