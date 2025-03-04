62bcbe28c706e7523a6bf69b6cc313f8b1eeeaa5
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-03-04 10:24:05 -0500

    Delete oci19 users

---
+++ migrations/03_delete_oci19_users.sql
@@ -0,0 +1,31 @@
+start transaction;
+
+create view oci19_user as
+select *
+from auth_user
+where username regexp '^oci19[a-z]+[0-9]+$';
+
+# select count(*)
+# from oci19_user;
+
+delete
+from judge_profile_organizations
+where profile_id in (select jp.id
+                     from judge_profile jp
+                              join oci19_user au on jp.user_id = au.id);
+
+delete
+from judge_organizationrequest
+where user_id in (select au.id from oci19_user au);
+
+delete
+from judge_profile
+where user_id in (select au.id from oci19_user au);
+
+delete
+from auth_user
+where id in (select au.id from oci19_user au);
+
+drop view oci19_user;
+
+commit;
