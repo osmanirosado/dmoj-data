d82edb0ea2c781698e2d762c22a0ea7cd00030e5
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-03-05 11:08:09 -0500

    Query to archive the revision data of oci users

--- migrations/04_archive_users_with_prefix_oci.sql
+++ migrations/04_archive_users_with_prefix_oci.sql
@@ -3,10 +3,10 @@
 
 start transaction;
 
-drop view if exists oci_users;
+drop view if exists oci_user;
 
 # OCI users mapping
-create view oci_users as
+create view oci_user as
 select pu.username as primary_username,
        su.username as secondary_username,
        pp.id       as primary_profile_id,
@@ -18,50 +18,60 @@ from auth_user pu
          join judge_profile pp on pu.id = pp.user_id
          join judge_profile sp on su.id = sp.user_id;
 
-select *
-from oci_users;
+# select *
+# from oci_users;
 
 # Archive new contest participation
 update judge_contestparticipation t
 set user_id = (select secondary_profile_id
-               from oci_users
+               from oci_user
                where t.user_id = primary_profile_id)
 where user_id in (select primary_profile_id
-                  from oci_users);
+                  from oci_user);
 
 
 # Archive new banned users
 update judge_contest_banned_users t
 set profile_id = (select secondary_profile_id
-                  from oci_users
+                  from oci_user
                   where t.profile_id = primary_profile_id)
 where profile_id in (select primary_profile_id
-                     from oci_users);
+                     from oci_user);
 
 # Archive new submissions
 update judge_submission t
 set user_id = (select secondary_profile_id
-               from oci_users
+               from oci_user
                where t.user_id = primary_profile_id)
 where user_id in (select primary_profile_id
-                  from oci_users);
+                  from oci_user);
 
 # Archive new judge tickets
 update judge_ticket t
 set user_id = (select secondary_profile_id
-               from oci_users
+               from oci_user
                where t.user_id = primary_profile_id)
 where user_id in (select primary_profile_id
-                  from oci_users);
+                  from oci_user);
 
 # Archive new judge ticket messages
 update judge_ticketmessage t
 set user_id = (select secondary_profile_id
-               from oci_users
+               from oci_user
                where t.user_id = primary_profile_id)
 where user_id in (select primary_profile_id
-                  from oci_users);
+                  from oci_user);
 
-drop view oci_users;
+# Archive new reversion data
+update reversion_revision r
+set user_id = (select secondary_user_id
+               from oci_user
+               where r.user_id = primary_user_id)
+where user_id in (select primary_user_id
+                  from oci_user);
+
+drop view oci19_user;
+
+drop view oci_user;
 
 commit;
