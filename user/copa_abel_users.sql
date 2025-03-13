2d009b18e88dccf1edd0ed41b2061c4ea0879ed6
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-03-13 11:02:44 -0400

    Queries to get the users of Copa Abel

---
+++ user/copa_abel_users.sql
@@ -0,0 +1,25 @@
+-- Copa Abel users
+
+set @organization_short_name = 'Copa Abel' collate utf8mb4_uca1400_as_ci;
+set @primary_username_pattern = '^copa[0-9]+$' collate utf8mb4_uca1400_as_ci;
+set @secondary_username_pattern = '^_copa[0-9]+$' collate utf8mb4_uca1400_as_ci;
+select @secondary_username_pattern;
+
+-- Primary Users
+select username
+from auth_user
+where username regexp @primary_username_pattern;
+
+-- Organization
+select au.username, jo.short_name
+from judge_organization jo
+         join judge_profile_organizations jpo on jo.id = jpo.organization_id
+         join judge_profile jp on jpo.profile_id = jp.id
+         join auth_user au on jp.user_id = au.id
+where jo.short_name = @organization_short_name
+  and au.username regexp @primary_username_pattern;
+
+-- Secondary Users
+select username
+from auth_user
+where username regexp @secondary_username_pattern;
