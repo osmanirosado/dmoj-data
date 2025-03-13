2798cdc9e00e96d69857b1ae18305cdddbb61c62
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-03-13 16:10:59 -0400

    Get association of copa abel users to organizations

--- user/copa_abel_users.sql
+++ user/copa_abel_users.sql
@@ -1,6 +1,5 @@
 -- Copa Abel users
 
-set @organization_short_name = 'Copa Abel';
 set @primary_username_pattern = '^copa[0-9]+$';
 set @secondary_username_pattern = '^_copa[0-9]+$';
 select @secondary_username_pattern;
@@ -16,8 +15,7 @@ from judge_organization jo
          join judge_profile_organizations jpo on jo.id = jpo.organization_id
          join judge_profile jp on jpo.profile_id = jp.id
          join auth_user au on jp.user_id = au.id
-where jo.short_name = @organization_short_name
-  and au.username regexp @primary_username_pattern;
+where au.username regexp @primary_username_pattern;
 
 -- Secondary Users
 select username
