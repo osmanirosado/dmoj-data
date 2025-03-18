557ead862c7679394a1ca622e7b3e2927c179bad
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-03-18 15:32:21 -0400

    Queries to count references to a contest object

---
+++ contest/count_query_builder.sql
@@ -0,0 +1,30 @@
+drop function if exists build_count_query;
+
+delimiter //
+
+create function build_count_query(in referencing_table varchar(255), in referencing_column varchar(255))
+    returns varchar(512)
+begin
+    declare result varchar(512);
+
+    set @template = '
+        select count(*) as rows_count
+        from {{referencing_table}} s
+        where s.{{referencing_column}} = @contest_object_id;
+    ';
+
+    -- Replace placeholders with actual values
+    set @sql = replace(@template, '{{referencing_table}}', referencing_table);
+    set @sql = replace(@sql, '{{referencing_column}}', referencing_column);
+
+    select @sql into result;
+    return result;
+end //
+
+delimiter ;
+
+select build_count_query(table_name,
+                         column_name) as query
+from information_schema.key_column_usage
+where referenced_table_name = 'judge_contest'
+  and referenced_table_schema = 'dmoj';
