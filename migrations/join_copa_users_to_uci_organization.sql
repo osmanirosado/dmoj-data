e18a4f79f2eb0a4c4b4a961e303288daca4c88d8
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-03-27 10:19:40 -0400

    Query to review the organizations of 'copa' users

--- migrations/join_copa_users_to_uci_organization.sql
+++ migrations/join_copa_users_to_uci_organization.sql
@@ -11,6 +11,7 @@ from auth_user u
          join judge_profile p on u.id = p.user_id
 where u.username regexp '^copa[0-9]+$';
 
+-- Update sort value
 update judge_profile_organizations
 set sort_value = 2
 where profile_id in (select p.id
@@ -20,3 +21,12 @@ where profile_id in (select p.id
   and organization_id = (select id
                          from judge_organization
                          where short_name = 'Copa UCI');
+
+-- Review query
+select u.username, o.short_name, po.sort_value
+from auth_user u
+         join judge_profile p on u.id = p.user_id
+         join judge_profile_organizations po on p.id = po.profile_id
+         join judge_organization o on po.organization_id = o.id
+where u.username regexp '^copa[0-9]+$'
+order by u.username;
